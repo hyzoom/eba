7f2bb5204c36b1fb97cd95423a69f38c88d54268
hazem <hazem.abdalazeem@gmail.com>
2015-11-26 16:11:05 +0200

    EBA Project : 1st commit

---
+++ app/src/androidTest/java/fragment/EventFragment.java
@@ -0,0 +1,27 @@
+package fragment;
+
+/**
+ * Created by hazem on 11/26/15.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eba.R;
+
+/**
+ * Created by Admin on 04-06-2015.
+ */
+public class EventFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.events_fragment, container, false);
+        return v;
+    }
+}
